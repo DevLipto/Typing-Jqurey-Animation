91ec0624e878934980deb2d4891c601ce1cd30f7
DevLipto <mdlipto56@gmail.com>
2023-10-18 05:11:19 +0600

    Typing Jquery animation commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Typing-Jqurey-Animation
---
+++ function.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var typed = new Typed(".typedClass", {
+        strings: ["worpress theme developer", "also a plugin developer"],
+        typeSpeed: 80, 
+        backSpeed: 65, 
+        loop: true  
+    });
+})
